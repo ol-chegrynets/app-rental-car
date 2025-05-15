ca07d2ce8016e6157408367f59d5539c919f5fe7
shura73 <ol.chegrynets@gmail.com>
2025-05-15 23:56:06 +0200

    styles

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import s from './AppBar.module.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../Logo/Logo.jsx';
 
 const AppBar = () => {
   return (
     <header className={s.header}>
-      <div className={s.logoWrapper}>
-        <p className={s.textLogo}>
-          rental
-          <span className={s.textLogoSpan}>car</span>
-        </p>
-      </div>
-      <div className={s.navWrapper}>
-        <nav className={s.nav}>
-          <ul className={s.navList}>
-            <li className={s.navItem}>
-              <NavLink to="/" className={s.navLink}>
-                Home
-              </NavLink>
-            </li>
-            <li className={s.navItem}>
-              <NavLink to="/catalog" className={s.navLink}>
-                Catalog
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Logo />
+      <nav className={s.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? s.active : s.link)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) => (isActive ? s.active : s.link)}
+        >
+          Catalog
+        </NavLink>
+      </nav>
     </header>
   );
 };
