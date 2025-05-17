2acc700a67c42ee57f48bbf0d2073656586f0831
shura73 <ol.chegrynets@gmail.com>
2025-05-17 17:48:00 +0200

    hero

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './AppBar.module.css';
 import { NavLink } from 'react-router-dom';
-
+import Logo from '../Logo/Logo.jsx';
 
 const AppBar = () => {
   return (
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Homepage.module.css';
 import clsx from 'clsx';
+import Catalog from '../Catalog/Catalog.jsx';
 
 const Homepage = () => {
+  const [clicks, setClicks] = useState(false);
+
+  const handleClick = () => {
+    console.log('Button clicked');
+    setClicks(true);
+  };
   return (
-    <section className={clsx(s.homepage, 'container')}>
-      <input type="text" />
+    <section className={clsx(s.homePage, 'container')}>
+      <div className={s.homeWrapper}>
+        <div className={s.homeText}>
+          <h1 className={s.title}>Find your perfect rental car</h1>
+          <p className={s.text}>
+            Reliable and budget-friendly rentals for any journey
+          </p>
+        </div>
+        <button onClick={handleClick} className={s.button} type="button">
+          View&nbsp;Catalog
+        </button>
+        {clicks && <Catalog />}
+      </div>
     </section>
   );
 };
